670134c43f2ebda9ee67ee74325c2f094f234a5d
HARUN ŞİMŞEK <humann210@gmail.com>
2022-04-18 05:16:11 +0300

    son degisim

--- lib/relaxMenu.dart
+++ lib/relaxMenu.dart
@@ -54,7 +54,7 @@ class RelaxMenu extends StatelessWidget {
                   Padding(
                     padding: EdgeInsets.only(top: 60),
                     child: Text(
-                      "Relaxxx",
+                      "Relax",
                       style: TextStyle(
                         fontSize: 80,
                         fontWeight: FontWeight.bold,
